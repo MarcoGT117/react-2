80168c98685564310a48155d572d4a63c74d8fd2
Marco Gonzalez <marco.g.gleztorres@gmail.com>
2023-05-29 16:17:42 -0600

    Image searcher

--- image-searcher/src/App.js
+++ image-searcher/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { Formik, Form, Field } from "formik";
+import "./header.css";
+import "./content.css";
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [photos, setPhotos] = useState([]);
+  const open = url => window.open(url)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <header>
+        <Formik
+          initialValues={{ search: "" }}
+          onSubmit={async (values) => {
+            //Lllamada a API de unsplash
+            const response = await fetch(
+              // Segun la documentacion este el endpoint al que hay que mandar la solicitud de busqueda
+              `https://api.unsplash.com/search/photos?page=1&query=${values.search}`,
+              {
+                // Objeto de configuracion para la API
+                headers: {
+                  Authorization:
+                    "Client-ID geDtFxILV2XFkurPBso4vFgdEbmMh8k4CZtOtrOaOcs",
+                },
+              }
+            );
+            const data = await response.json();
+            console.log(data)
+            setPhotos(data.results);
+          }}
         >
-          Learn React
-        </a>
+          <Form>
+            <Field name="search" />
+          </Form>
+        </Formik>
       </header>
+      <div className="container">
+        <div className="center">      
+          {photos.map(photo => 
+            <article key={photo.id} onClick={()=> open(photo.links.html)}>
+              <img src={photo.urls.regular} alt={photo.alt_description}/>
+              <p>{photo.description === null ?  photo.alt_description : photo.description}</p>
+            </article>
+          )}
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
